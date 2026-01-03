ad8541bce6f2715a97731544c132496515b63153
gokilasundarraj <gokilas333@gmail.com>
2026-01-03 10:29:37 +0530

    axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5002/api',
+  baseURL: 'https://survery-1.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
