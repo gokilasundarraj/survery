d3e68387d4ac67f87f73a381912f81cb9b21b68a
gokilasundarraj <gokilas333@gmail.com>
2026-01-03 09:55:18 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
 
@@ -11,25 +11,25 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("Mongo error:", err));
+  .catch((err) => {
+    console.error("MongoDB connection error:");
+    console.error(err.message);
+  });
 
-
+// Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/admin", require("./routes/admin"));
 app.use("/api/user", require("./routes/user"));
 app.use("/api/surveys", require("./routes/surveyRoutes"));
 
-
 app.get("/", (req, res) => {
-  res.send("Backend running 🚀");
+  res.send("Backend running ");
 });
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(` Server running on port ${PORT}`);
 });
